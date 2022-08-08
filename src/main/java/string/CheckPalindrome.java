42416bb54890f2e024e0649a0cdcbb8fad06e2d9
sobanjawaid26 <sobanjawaid26@gmail.com>
2022-08-08 18:18:54 +0530

    string palindrome recursive done

--- src/main/java/string/CheckPalindrome.java
+++ src/main/java/string/CheckPalindrome.java
@@ -12,12 +12,29 @@ public class CheckPalindrome {
     }
 
     public static boolean isPalindromeRecursive(String str){
+        int n = str.length();
+        if(n == 0)
+            return true;
+        return isPalRec(str, 0 , n-1);
+    }
 
-        return false;
+    private static boolean isPalRec(String str, int s, int e) {
+        // If there is only one character
+        if(s == e)
+            return true;
+        // If first and last characters do not match
+        if(str.charAt(s) != str.charAt(e))
+            return false;
+        // If there are more than two characters, check if middle substring is also palindrome or not.
+        if(s < e + 1)
+            isPalRec(str, s+1, e-1);
+
+        return true;
     }
 
     public static void main(String[] args) {
         String str = "abccba";
         System.out.println("String "+ str + " is palindrome : " + CheckPalindrome.isPalindromeIterative(str));
+        System.out.println("String "+ str + " is palindrome : " + CheckPalindrome.isPalindromeRecursive(str));
     }
 }
