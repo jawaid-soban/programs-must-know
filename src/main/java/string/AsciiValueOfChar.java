bb347060d079aacf6975946c5fea883c8ec59d14
sobanjawaid26 <sobanjawaid26@gmail.com>
2022-08-09 22:54:34 +0530

    Find the ASCII value of a character done

---
+++ src/main/java/string/AsciiValueOfChar.java
@@ -0,0 +1,13 @@
+package string;
+
+public class AsciiValueOfChar {
+
+    public static int value(char c){
+        int val = c;
+        return val;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(AsciiValueOfChar.value('Z'));
+    }
+}
