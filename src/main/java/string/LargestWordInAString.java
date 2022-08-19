ed1eac055efdcc12e5ff8f0b7a6c1b6f0a7fdb15
sobanjawaid26 <sobanjawaid26@gmail.com>
2022-08-19 08:03:34 +0530

    Find the largest word in a String

---
+++ src/main/java/string/LargestWordInAString.java
@@ -0,0 +1,37 @@
+package string;
+
+public class LargestWordInAString {
+
+    public static String largest(String str){
+        int len = str.length();
+        int i = 0, j = 0;
+        String maxWord="";
+        int max_length = 0, max_start = 0;
+
+        while (j <= len) {
+            if (j < len && str.charAt(j) != ' ')
+                j++;
+
+            else {
+                int curr_length = j - i;
+
+                if (curr_length > max_length) {
+                    max_length = curr_length;
+                    max_start = i;
+                }
+                j++;
+                i = j;
+            }
+        }
+
+        maxWord = str.substring(max_start, max_length);
+        return maxWord;
+    }
+
+    public static void main(String[] args) {
+        String str= "Google Doc";
+        System.out.println(
+                LargestWordInAString.largest(str)
+        );
+    }
+}
